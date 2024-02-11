5b595d36a149e610a887361e86adbfe3a8ad229b
Graham Cummins <grahamiancummins@gmail.com>
2024-02-11 12:04:21 -0700

    stash

---
+++ pgp/build.gradle.kts
@@ -0,0 +1,49 @@
+
+val ktorVersion: String by rootProject.extra("")
+
+
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    alias(libs.plugins.jvm)
+
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use the Kotlin JUnit 5 integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // Use the JUnit 5 integration.
+    testImplementation(libs.junit.jupiter.engine)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // This dependency is used by the application.
+    implementation(libs.guava)
+    implementation(libs.jackson.jsr310)
+    implementation(libs.jackson.kotlin)
+    implementation(libs.bouncycastle)
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("idoubtthat.server.AppKt")
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
